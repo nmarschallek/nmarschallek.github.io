c9d2d3770f832cc0b97052de79c6f1f4a0ed59ae
nmarschallek <38546031+nmarschallek@users.noreply.github.com>
2021-08-05 14:01:10 +1000

    rewrite entire site

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,97 @@
 <template>
-  <v-app style="margin-top: 0px; background-color: #44546A;">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm6 md3>
-            <v-card flat color="secondary" style="border-radius: 0px;">
-              <v-toolbar flat color="accent">
-                <v-toolbar-title style="color: #404040;">Nikolaus Marschallek</v-toolbar-title>
-              </v-toolbar>
+  <div class="bg-gray-100 h-screen">
+    <div class="space-y-5 bg-gray-200 shadow-md">
+      <div class="container mx-auto flex flex-col space-y-3 py-12 px-5">
+        <div class="flex sm:flex-row flex-col">
+          <div class="w-56 h-56 m-5 mx-auto sm:mx-5">
+            <img src="./assets/headshot.png" class="rounded-full border border-4 border-white">
+          </div>
+          <div class="my-auto flex flex-col space-y-1">
+            <h1 class="text-5xl text-gray-800">Nikolaus Marschallek</h1>
+            <span class="text-xl text-indigo-800 font-semibold tracking-wide">Accounting & Finance Student</span>
+            <div class="flex flex-row space-x-5 pt-3">
+              <a
+                v-for="link in links"
+                :href="decode(link.url)"
+                target="_blank"
+                class="bg-indigo-800 text-white py-1 px-5 rounded-md flex flex-row space-x-2 hover:shadow-xl"
+              >
+                <svg class="my-auto" width="16" height="16" :viewBox="link.svgViewBox" v-html="link.svg"></svg>
+                <span>{{ link.text }}</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="text-gray-500">
+          <p>
+            Hi, I'm Nik and I'm a final year student at the Queensland University of Technology, studying a Bachelor of Business (Accounting and Finance). Here are some of the side projects that I've worked on recently.
+          </p>
+        </div>
+      </div>
+    </div>
 
-              <v-card-text>
-                <v-list two-line color="secondary">
-                  <v-list-item
-                    hover
-                    :href="decode(item.link)"
-                    target="_blank"
-                    v-for="item in items"
-                    :key="item.title"
-                  >
-                    <v-list-item-avatar>
-                      <font-awesome-icon
-                        :icon="[item.prefix, item.icon]"
-                        size="3x"
-                        :style="{ color: '404040' }"
-                      />
-                    </v-list-item-avatar>
+    <div class="container mx-auto pb-10 px-5">
+      <h2 class="text-2xl font-semibold tracking-wide text-gray-800 py-5">Projects</h2>
+      <ul v-if="projects.length" class="space-y-5">
+        <a
+          v-for="project in projects"
+          :href="project.url"
+          target="_blank"
+          class="bg-white shadow-lg rounded-md p-3 flex flex-row space-x-5 h-52 hover:shadow-xl"
+        >
+          <div class="bg-blue-300 w-2/3 h-full">
 
-                    <v-list-item-content>
-                      <v-list-item-title v-html="item.title" style="color: #404040;"></v-list-item-title>
-                      <v-list-item-subtitle v-html="decode(item.linkText)" style="color: #404040;"></v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+          </div>
+          <div>
+            <h3 class="text-xl text-indigo-800 font-semibold tracking-wide py-1">{{ project.title }}</h3>
+            <p class="text-gray-500">{{ project.body }}</p>
+          </div>
+        </a>
+      </ul>
+      <span v-else class="text-gray-500 italic">There are currently no projects listed.</span>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  data: () => ({
-    items: [
-      {
-        title: 'Mobile',
-        linkText: 'MDQyNiA4NDEgMjQ2',
-        link: 'dGVsOjA0MjY4NDEyNDY=',
-        icon: 'phone',
-        prefix: 'fas'
-      },
-      {
-        title: 'Email',
-        linkText: 'bmlrb2xhdXNAbWFyc2NoYWxsZWsubWU=',
-        link: 'bWFpbHRvOm5pa29sYXVzQG1hcnNjaGFsbGVrLm1l',
-        icon: 'envelope',
-        prefix: 'fas'
-      },
-      {
-        title: 'Website',
-        linkText: 'bWFyc2NoYWxsZWsubWU=',
-        link: 'aHR0cHM6Ly9tYXJzY2hhbGxlay5tZQ==',
-        icon: 'globe-asia',
-        prefix: 'fas'
-      },
-      {
-        title: 'Facebook Messenger',
-        linkText: 'bS5tZS9uaWttYXJzY2hhbGxlaw==',
-        link: 'aHR0cHM6Ly9tLm1lL25pa21hcnNjaGFsbGVr',
-        icon: 'facebook-messenger',
-        prefix: 'fab'
-      },
-      {
-        title: 'LinkedIn',
-        linkText: 'bGlua2VkaW4uY29tL2luL25tYXJzY2hhbGxlaw==',
-        link: 'aHR0cHM6Ly9saW5rZWRpbi5jb20vaW4vbm1hcnNjaGFsbGVrLw==',
-        icon: 'linkedin',
-        prefix: 'fab'
-      },
-      {
-        title: 'Github',
-        linkText: 'Z2l0aHViLmNvbS9ubWFyc2NoYWxsZWs=',
-        link: 'aHR0cHM6Ly9naXRodWIuY29tL25tYXJzY2hhbGxlaw==',
-        icon: 'github',
-        prefix: 'fab'
-      }
-    ]
-  }),
-  methods: {
-    decode: (string) => {
-      return atob(string)
-    }
-  }
-};
+<script setup>
+import { reactive } from "vue"
+
+let projects = reactive([])
+let links = reactive([
+  {
+    text: 'Email',
+    url: 'bWFpbHRvOm5pa29sYXVzQG1hcnNjaGFsbGVrLm1l',
+    svg: `
+        <g fill="#fff">
+            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+        </g>
+    `,
+    svgViewBox: '0 0 20 20',
+  },
+  {
+    text: 'LinkedIn',
+    url: 'aHR0cHM6Ly9saW5rZWRpbi5jb20vaW4vbm1hcnNjaGFsbGVr',
+    svg: `
+        <g fill="none">
+            <path d="M0 18.338C0 8.216 8.474 0 18.92 0h218.16C247.53 0 256 8.216 256 18.338v219.327C256 247.79 247.53 256 237.08 256H18.92C8.475 256 0 247.791 0 237.668V18.335z" fill="#fff"/>
+            <path d="M77.796 214.238V98.986H39.488v115.252H77.8zM58.65 83.253c13.356 0 21.671-8.85 21.671-19.91-.25-11.312-8.315-19.915-21.417-19.915-13.111 0-21.674 8.603-21.674 19.914 0 11.06 8.312 19.91 21.169 19.91h.248zM99 214.238h38.305v-64.355c0-3.44.25-6.889 1.262-9.346 2.768-6.885 9.071-14.012 19.656-14.012 13.858 0 19.405 10.568 19.405 26.063v61.65h38.304v-66.082c0-35.399-18.896-51.872-44.099-51.872-20.663 0-29.738 11.549-34.78 19.415h.255V98.99H99.002c.5 10.812-.003 115.252-.003 115.252z" fill="rgba(55, 48, 163, var(--tw-bg-opacity))"/>
+        </g>
+    `,
+    svgViewBox: '0 0 256 249',
+  },
+  {
+    text: 'Github',
+    url: 'aHR0cHM6Ly9naXRodWIuY29tL25tYXJzY2hhbGxlaw==',
+    svg: `
+        <g fill="#fff">
+            <path d="M127.505 0C57.095 0 0 57.085 0 127.505c0 56.336 36.534 104.13 87.196 120.99 6.372 1.18 8.712-2.766 8.712-6.134 0-3.04-.119-13.085-.173-23.739-35.473 7.713-42.958-15.044-42.958-15.044-5.8-14.738-14.157-18.656-14.157-18.656-11.568-7.914.872-7.752.872-7.752 12.804.9 19.546 13.14 19.546 13.14 11.372 19.493 29.828 13.857 37.104 10.6 1.144-8.242 4.449-13.866 8.095-17.05-28.32-3.225-58.092-14.158-58.092-63.014 0-13.92 4.981-25.295 13.138-34.224-1.324-3.212-5.688-16.18 1.235-33.743 0 0 10.707-3.427 35.073 13.07 10.17-2.826 21.078-4.242 31.914-4.29 10.836.048 21.752 1.464 31.942 4.29 24.337-16.497 35.029-13.07 35.029-13.07 6.94 17.563 2.574 30.531 1.25 33.743 8.175 8.929 13.122 20.303 13.122 34.224 0 48.972-29.828 59.756-58.22 62.912 4.573 3.957 8.648 11.717 8.648 23.612 0 17.06-.148 30.791-.148 34.991 0 3.393 2.295 7.369 8.759 6.117 50.634-16.879 87.122-64.656 87.122-120.973C255.009 57.085 197.922 0 127.505 0"/>
+            <path d="M47.755 181.634c-.28.633-1.278.823-2.185.389-.925-.416-1.445-1.28-1.145-1.916.275-.652 1.273-.834 2.196-.396.927.415 1.455 1.287 1.134 1.923M54.027 187.23c-.608.564-1.797.302-2.604-.589-.834-.889-.99-2.077-.373-2.65.627-.563 1.78-.3 2.616.59.834.899.996 2.08.36 2.65M58.33 194.39c-.782.543-2.06.034-2.849-1.1-.781-1.133-.781-2.493.017-3.038.792-.545 2.05-.055 2.85 1.07.78 1.153.78 2.513-.019 3.069M65.606 202.683c-.699.77-2.187.564-3.277-.488-1.114-1.028-1.425-2.487-.724-3.258.707-.772 2.204-.555 3.302.488 1.107 1.026 1.445 2.496.7 3.258M75.01 205.483c-.307.998-1.741 1.452-3.185 1.028-1.442-.437-2.386-1.607-2.095-2.616.3-1.005 1.74-1.478 3.195-1.024 1.44.435 2.386 1.596 2.086 2.612M85.714 206.67c.036 1.052-1.189 1.924-2.705 1.943-1.525.033-2.758-.818-2.774-1.852 0-1.062 1.197-1.926 2.721-1.951 1.516-.03 2.758.815 2.758 1.86M96.228 206.267c.182 1.026-.872 2.08-2.377 2.36-1.48.27-2.85-.363-3.039-1.38-.184-1.052.89-2.105 2.367-2.378 1.508-.262 2.857.355 3.049 1.398"/>
+        </g>
+    `,
+    svgViewBox: '0 0 256 249',
+  },
+])
+
+let decode = (string) => atob(string)
 </script>
--- src/main.js
+++ src/main.js
@@ -1,32 +1,6 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-    faPhone,
-    faEnvelope,
-    faGlobeAsia
-  } from '@fortawesome/free-solid-svg-icons'
-import {
-    faFacebookMessenger,
-    faLinkedin,
-    faGithub
-  } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './index.css'
 
-library.add(faPhone)
-library.add(faEnvelope)
-library.add(faGlobeAsia)
-library.add(faFacebookMessenger)
-library.add(faLinkedin)
-library.add(faGithub)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.config.productionTip = false
-
-new Vue({
-  vuetify,
-  components: { App },
-  render: h => h(App)
-}).$mount('#app')
+createApp(App)
+  .mount('#app')
